f939e6664d363b7070c7ec634dee8f4577ed643f
Gustavo Lopez <gml.gustavoml@gmail.com>
2020-09-27 03:06:17 -0300

    Contact data was got from the data file

--- src/data/contact.data.js
+++ src/data/contact.data.js
@@ -1,1 +1,15 @@
-export const Contact_Data = {};
+export const Contact_Data = {
+  mainTitle: 'Contacto',
+  inputs: [
+    { title: 'Nombre' },
+    { title: 'Apellido' },
+    { title: 'Email' },
+    { title: 'Telefono' }
+  ],
+  textArea: {
+    title: 'Mensaje'
+  },
+  buttonSend: {
+    text: 'Enviar'
+  }
+};
